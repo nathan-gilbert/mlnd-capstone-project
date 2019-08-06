1be94abf70b805113e2228eb2759fce66544fca0
Nathan Gilbert <nathan.gilbert@gmail.com>
2019-08-05 19:10:36 -0600

    Adding in skeleton project

---
+++ index.py
@@ -0,0 +1,4 @@
+from summerizer.summerizer import Summerizer
+
+if __name__ == "__main__":
+    summer = Summerizer()
--- summerizer/summerizer.py
+++ summerizer/summerizer.py
@@ -1,3 +1,11 @@
 
+
 class Summerizer:
-    pass
+    def __init__(self):
+        self.__model = None
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
