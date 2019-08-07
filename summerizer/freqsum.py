86aef3b1618d51b88d4392d7a7db89860c249e70
Nathan Gilbert <nathan.gilbert@gmail.com>
2019-08-07 14:27:55 -0600

    add files for implementations of sum types

---
+++ summerizer/freqsum.py
@@ -0,0 +1,12 @@
+from summerizer.summerizer import Summerizer
+
+
+class FreqSum(Summerizer):
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
---
+++ summerizer/regsum.py
@@ -0,0 +1,12 @@
+from summerizer.summerizer import Summerizer
+
+
+class RegSum(Summerizer):
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
