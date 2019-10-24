d8d104ef8926b6b45deb6846636ecd3913310ceb
Nathan Gilbert <nathan.gilbert@gmail.com>
2019-10-23 19:28:16 -0600

    A few pylint fixes

--- summerizer/regsum.py
+++ summerizer/regsum.py
@@ -36,19 +36,21 @@ class RegSum(Summerizer):
         top_1000_counts = []
         for doc_set in all_training_doc_sets.values():
             doc_set.create_word_probabilities()
+
             # create global word counts for LLR
-            for word, count in doc_set.word_counts.items():
-                global_counts = global_counts.get(word, 0) + count
+            # for word, count in doc_set.word_counts.items():
+            #    global_counts = global_counts.get(word, 0) + count
 
             # get the top 1000 words in the training set
             top_1000_counts = sorted(global_counts.items(),
                                      key=operator.itemgetter(1),
                                      reverse=True)[:1000]
-        self.__unsupervised_features(all_training_doc_sets, global_counts, top_1000_counts)
+
+        self.__unsupervised_features(all_training_doc_sets, top_1000_counts)
         # self.__word_location_features(all_training_doc_sets)
         # self.__word_type_features(all_training_doc_sets)
 
-    def __unsupervised_features(self, all_docs_sets, global_counts, top_k_counts):
+    def __unsupervised_features(self, all_docs_sets, top_k_counts):
         # FreqSum - take the top K words from training data
         for doc_set in all_docs_sets:
             for doc in doc_set:
@@ -66,7 +68,8 @@ class RegSum(Summerizer):
 
                 # TextRank
                 sentences = doc.annotations["sentences"]
-                ranked_sentences = self.__get_text_rank(sentences)
+                ranked_sentences = self.__get_text_rank(sentences)[:3]
+
 
     def __word_location_features(self, all_docs):
         # 6 types
@@ -85,29 +88,32 @@ class RegSum(Summerizer):
     def __get_labels(self):
         pass
 
+    # pylint: disable=too-many-locals
     def __get_text_rank(self, sentences):
         """
-        Generate a ranked list of sentences from the document that are ranked by the PageRank algorithm.
+        Generate a ranked list of sentences from the document that are ranked
+        by the PageRank algorithm.
         :return: ranked_sentences
         """
         # Extract word vectors
         word_embeddings = {}
-        f = open('glove.6B.100d.txt', encoding='utf-8')
-        for line in f:
-            values = line.split()
-            word = values[0]
-            coefs = np.asarray(values[1:], dtype='float32')
-            word_embeddings[word] = coefs
-        f.close()
+        with open('glove.6B.100d.txt', encoding='utf-8') as in_file:
+            for line in in_file:
+                values = line.split()
+                word = values[0]
+                coefs = np.asarray(values[1:], dtype='float32')
+                word_embeddings[word] = coefs
 
         clean_sentences = [self._remove_stop_words(r.split()) for r in sentences]
         sentence_vectors = []
         for i in clean_sentences:
             if len(i) != 0:
-                v = sum([word_embeddings.get(w, np.zeros((100,))) for w in i.split()]) / (len(i.split()) + 0.001)
+                s_v = sum(
+                    [word_embeddings.get(w, np.zeros((100,))) for w in i.split()]
+                ) / (len(i.split()) + 0.001)
             else:
-                v = np.zeros((100,))
-            sentence_vectors.append(v)
+                s_v = np.zeros((100,))
+            sentence_vectors.append(s_v)
 
         # similarity matrix
         sim_mat = np.zeros([len(sentences), len(sentences)])
